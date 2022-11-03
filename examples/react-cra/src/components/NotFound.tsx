8db45e7b100b110aa145e7a74f4d0219e9b78e6c
Evgeny Bereza <bereza@greensight.ru>
2022-11-03 16:46:47 +0300

    #94010 Dist add to git. Demo and examples folder has been removed

--- examples/react-cra/src/components/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const NotFound = () => {
-  return (
-    <div>
-      <p>Error: Page does not exist!</p>
-    </div>
-  );
-};
--- examples/react-cra/src/components/StoplightProject.tsx
+++
@@ -1,8 +0,0 @@
-import '@stoplight/elements/styles.min.css';
-
-import { StoplightProject } from '@stoplight/elements-dev-portal';
-import React from 'react';
-
-export const StoplightProjectDocs: React.FC = () => {
-  return <StoplightProject basePath="stoplight-project" platformUrl="https://stoplight.io" projectId="cHJqOjYwNjYx" />;
-};
